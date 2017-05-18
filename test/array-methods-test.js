96d4ebd4b8c215618e5a156dbc8fa926e76044d7
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-18 10:16:31 -0700

    map tests failing

--- test/array-methods-test.js
+++ test/array-methods-test.js
@@ -1,12 +1,12 @@
 const assert = require('chai').assert;
 const forEach = require('../array-methods');
 
-let elements = [];
-let index = [];
-
-const animals = ['cats', undefined, 'dogs', 'bears'];
 
 describe('forEach', () => {
+  const animals = ['cats', undefined, 'dogs', 'bears'];
+  let elements = [];
+  let index = [];
+
   it('calls the callback once for each item in the array', () => {
     forEach(animals, (element) => elements.push(element));
     
@@ -25,5 +25,20 @@ describe('forEach', () => {
     returnValue = forEach(animals, (element) => index.push(element));
 
     assert.equal(returnValue, undefined);
+  });
+});
+
+describe('map', () => {
+  const numbers = [1, undefined, 2, 3];
+  let doubles = null;
+
+  it('creates a new array with the return value of each called callback', () => {
+    doubles = map(numbers, (element) => element * 2);
+
+    assert.deepEqual(doubles, [2, undefined, 4, 6]);
+  });
+
+  it('returns undefined element in return array if original element is undefined', () => {
+    assert.equal(doubles[1], undefined);
   });
 });
