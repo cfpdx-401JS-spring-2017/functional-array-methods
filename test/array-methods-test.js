8de4c2cdd4fb7639c213dfd6165d44aa6d5f2006
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-17 18:18:55 -0700

    forEach test failing

--- test/array-methods-test.js
+++ test/array-methods-test.js
@@ -1,2 +1,13 @@
 const assert = require('chai').assert;
 
+let forEachArr = [];
+
+const animals = ['cats', 'dogs', 'bears'];
+
+describe('forEach', () => {
+  it('calls the callback once for each item in the array', () => {
+    forEach(animals, push);
+    
+    assert.equal(forEachArr.length, 3);
+  });
+});
