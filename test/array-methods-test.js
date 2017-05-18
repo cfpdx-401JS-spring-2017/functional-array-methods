85fe453318f2f684a6db28c93d781fd0e76c929e
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-18 09:46:49 -0700

    forEach index tests passing

--- test/array-methods-test.js
+++ test/array-methods-test.js
@@ -1,14 +1,22 @@
 const assert = require('chai').assert;
 const forEach = require('../array-methods');
 
-let forEachArr = [];
+let elements = [];
+let index = [];
 
 const animals = ['cats', undefined, 'dogs', 'bears'];
 
 describe('forEach', () => {
   it('calls the callback once for each item in the array', () => {
-    forEach(animals, (element) => forEachArr.push(element));
+    forEach(animals, (element) => elements.push(element));
     
-    assert.equal(forEachArr.length, 3);
+    assert.equal(elements.length, 3);
+  });
+
+  it('pushes the index into the array for each element', () => {
+    forEach(animals, (element, idx) => index.push(idx));
+    
+    assert.equal(index.length, 3);
+    assert.equal(index[2], 3);
   });
 });
