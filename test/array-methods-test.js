b6d2587dfa3083756700e0f85cf203b12dffc6c2
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-18 10:04:14 -0700

    forEach returns undefined test passing

--- test/array-methods-test.js
+++ test/array-methods-test.js
@@ -15,8 +15,15 @@ describe('forEach', () => {
 
   it('pushes the index into the array for each element', () => {
     forEach(animals, (element, idx) => index.push(idx));
-    
+
     assert.equal(index.length, 3);
     assert.equal(index[2], 3);
+  });
+
+  it('returns undefined', () => {
+    let returnValue = null;
+    returnValue = forEach(animals, (element) => index.push(element));
+
+    assert.equal(returnValue, undefined);
   });
 });
