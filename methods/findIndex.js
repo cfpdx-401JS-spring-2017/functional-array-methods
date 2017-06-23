7ea8f39b3394b3182979b59c7be92e16eed5a667
Morgan Fogarty <mofo37@gmail.com>
2017-06-22 19:49:57 -0700

    -1 test passing

--- methods/findIndex.js
+++ methods/findIndex.js
@@ -2,10 +2,11 @@ function findIndex(array, callback) {
   for (var i = 0; i < array.length; i++) {
     if (array.hasOwnProperty(i)) {
       if (callback(array[i], i) === true){
-        return array[i];
+        return i;
       }
     }
   }
+  return -1;
 }
 
 
--- test/findIndex.test.js
+++ test/findIndex.test.js
@@ -2,14 +2,14 @@ const chai = require('chai');
 const assert = chai.assert;
 const findIndex = require('../methods/findIndex');
 
-describe('findIndex', () => {
+describe.only('findIndex', () => {
 
   it('return first even numbers', () => {
     const array = [1, 2, 3, 4, 5, 6];
     let result = findIndex(array, (item, index) => {
       return !(item % 2);
     });
-    assert.deepEqual(result, 2);
+    assert.deepEqual(result, 1);
   });
 
   it('ignores hole', () => {
@@ -17,14 +17,26 @@ describe('findIndex', () => {
     let result = findIndex(array, (item, index) => {
       return !(item % 2);
     });
-    assert.deepEqual(result, 2);
+    assert.deepEqual(result, 0);
   });
 
   it('does not ignore undefined', () => {
-    const array = [2, undefined, 4];
+    const array = [1, undefined, 2, 4];
     let result = findIndex(array, (item, index) => {
-      return !(item % 2);
+      if (item !== undefined) {
+        return !(item % 2);
+      } 
     });
     assert.deepEqual(result, 2);
+  });
+
+  it('returns -1 if no truthy item is found', () => {
+    const array = [1, 3, 5, 7];
+    let result = findIndex(array, (item, index) => {
+      if (item !== undefined) {
+        return !(item % 2);
+      } 
+    });
+    assert.deepEqual(result, -1);
   });
 });
