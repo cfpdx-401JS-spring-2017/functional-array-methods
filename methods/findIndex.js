82da6b2fa66e84a9095e0afc5c7c0885b7d749c1
Morgan Fogarty <mofo37@gmail.com>
2017-06-22 18:50:57 -0700

    findIndex tests failing

---
+++ methods/findIndex.js
@@ -0,0 +1,16 @@
+function findIndex(array, callback) {
+  // const newArray = [];
+  // for (var i = 0; i < array.length; i++) {
+  //   if (array.hasOwnProperty(i) && array[i] !== undefined) {
+  //     if (callback(array[i], i) === true){
+  //       newArray.push(array[i]);
+  //     }
+  //   }
+  // }
+  // return newArray;
+}
+
+
+
+module.exports = findIndex;
+
---
+++ test/findIndex.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const assert = chai.assert;
+const findIndex = require('../methods/findIndex');
+
+describe.only('findIndex', () => {
+
+  it('return first even numbers', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    let result = findIndex(array, (item, index) => {
+      return !(item % 2);
+    });
+    assert.deepEqual(result, 2);
+  });
+
+  it('ignores hole', () => {
+    const array = [2, , 4];
+    let result = findIndex(array, (item, index) => {
+      return !(item % 2);
+    });
+    assert.deepEqual(result, 2);
+  });
+
+  it('ignores undefined', () => {
+    const array = [2, undefined, 4];
+    let result = findIndex(array, (item, index) => {
+      return !(item % 2);
+    });
+    assert.deepEqual(result, 2);
+  });
+});
