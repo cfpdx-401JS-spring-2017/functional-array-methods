af29d341f4b7386d7a8ca094b33274cb6aecc2fd
Morgan Fogarty <mofo37@gmail.com>
2017-06-22 18:40:14 -0700

    filter test passing

--- test/filter.test.js
+++ test/filter.test.js
@@ -2,29 +2,29 @@ const chai = require('chai');
 const assert = chai.assert;
 const filter = require('../methods/filter');
 
-describe.only('filter', () => {
+describe('filter', () => {
 
-  it('performs method on each item of array and returns an array', () => {
+  it('return only even numbers', () => {
     const array = [1, 2, 3, 4, 5, 6];
     let na = filter(array, (item, index) => {
-      return item % 2;
+      return !(item % 2);
     });
     assert.deepEqual(na, [2, 4, 6]);
   });
 
-  it('performs function on each valid item in array', () => {
+  it('ignores hole', () => {
     const array = [2, , 4];
-    filter(array, (item, index) => {
-      return item % 2;
+    let na = filter(array, (item, index) => {
+      return !(item % 2);
     });
-    assert.deepEqual(array, [2, 4]);
+    assert.deepEqual(na, [2, 4]);
   });
 
-  it('performs function on each item in array, including undefined', () => {
+  it('ignores undefined', () => {
     const array = [2, undefined, 4];
-    filter(array, (item, index) => {
-      return item % 2;
+    let na = filter(array, (item, index) => {
+      return !(item % 2);
     });
-    assert.deepEqual(array, [2, 4]);
+    assert.deepEqual(na, [2, 4]);
   });
 });
