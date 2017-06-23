05dc0d07d4478e5200d154577a4a5ad3e4290eb9
Morgan Fogarty <mofo37@gmail.com>
2017-06-22 18:20:04 -0700

    map test passing

--- test/map.test.js
+++ test/map.test.js
@@ -4,18 +4,18 @@ const map = require('../methods/map');
 
 describe('map', () => {
 
-  it.only('performs method on each item of array and returns an array', () => {
-    const array = [1, 2, 3];
-    na = map(array, (item, index) => {
-      array[index] = item + 1;
+  it('performs method on each item of array and returns an array', () => {
+    const array = [10, 12, 14];
+    let na = map(array, (item, index) => {
+      return array[index] = item + 1;
     });
-    assert.deepEqual(na, [2, 3, 4]);
+    assert.deepEqual(na, [11, 13, 15]);
   });
 
   it('performs function on each valid item in array', () => {
     const array = [2, , 4];
     map(array, (item, index) => {
-      array[index] = item + 1;
+      return array[index] = item + 1;
     });
     assert.deepEqual(array, [3, , 5]);
   });
@@ -23,7 +23,7 @@ describe('map', () => {
   it('performs function on each item in array, including undefined', () => {
     const array = [2, undefined, 4];
     map(array, (item, index) => {
-      array[index] = item + 1;
+      return array[index] = item + 1;
     });
     assert.deepEqual(array, [3, undefined, 5]);
   });
