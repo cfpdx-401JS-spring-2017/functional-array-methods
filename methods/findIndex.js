aa23dce812617cbbecc29d323ae43cae61158ba2
Morgan Fogarty <mofo37@gmail.com>
2017-06-22 18:55:18 -0700

    findIndex tests passing

--- methods/findIndex.js
+++ methods/findIndex.js
@@ -1,13 +1,11 @@
 function findIndex(array, callback) {
-  // const newArray = [];
-  // for (var i = 0; i < array.length; i++) {
-  //   if (array.hasOwnProperty(i) && array[i] !== undefined) {
-  //     if (callback(array[i], i) === true){
-  //       newArray.push(array[i]);
-  //     }
-  //   }
-  // }
-  // return newArray;
+  for (var i = 0; i < array.length; i++) {
+    if (array.hasOwnProperty(i) && array[i] !== undefined) {
+      if (callback(array[i], i) === true){
+        return array[i];
+      }
+    }
+  }
 }
 
 
